66e178a70e60734ad397f46c331d59a91754be0f
Amal <amalanuradha01@gmail.com>
2020-08-29 14:31:57 +0530

    latest

--- src/views/pages/Home.vue
+++ src/views/pages/Home.vue
@@ -1,11 +1,22 @@
 <template>
     <div>
         <siteHeader/>
-        <carousal/>
+        <div style="position: relative;">
+        <carousal>
+            
+        </carousal>
+        <h1 style="position: absolute;top: 3em;left: 3em;color: white;font-size: 3vw;">
+        
+            NO #01 Delivery Service <br> in Sri Lanka.
+        
+        </h1>
+        </div>
+       
         <siteFooter/>
     </div>
 </template>
 <script>
+import VueResponsiveText from 'vue-responsive-text'
 import siteFooter from '../../containers/Site/Footer'
 import siteHeader from '../../containers/Site/Header'
 import carousal from '../widgets/carousal'
@@ -13,7 +24,7 @@ export default {
     
     name: 'siteHome',
     components: {
-      siteFooter, siteHeader , carousal 
+      siteFooter, siteHeader , carousal , VueResponsiveText
     }
 
 }
