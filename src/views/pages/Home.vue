a4d1bc1c0d75cf535d1b260b25795aeb7785ef70
Amal <amalanuradha01@gmail.com>
2020-08-29 14:50:11 +0530

    d no responsive

--- src/views/pages/Home.vue
+++ src/views/pages/Home.vue
@@ -5,11 +5,20 @@
         <carousal>
             
         </carousal>
+        <div class="w-50" style="position: absolute;left: 3em;top: 6em;font-size: 3vw;">
+        <div class="input-group">
+       <input type="text" class="form-control" placeholder="Delivery Reference Number" aria-label="Delivery Reference Number" aria-describedby="button-addon2">
+  <div class="input-group-append">
+    <button class="btn btn-outline-secondary" type="button" id="button-addon2">Track</button>
+  </div>
+</div>
+        </div>
         <h1 style="position: absolute;top: 3em;left: 3em;color: white;font-size: 3vw;">
         
             NO #01 Delivery Service <br> in Sri Lanka.
         
         </h1>
+       
         </div>
        
         <siteFooter/>
