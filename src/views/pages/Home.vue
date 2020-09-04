d9fca03493da5aa674d2f062ac3652f0f050d524
Amal <amalanuradha01@gmail.com>
2020-09-04 20:52:29 +0530

    home banner text

--- src/views/pages/Home.vue
+++ src/views/pages/Home.vue
@@ -7,20 +7,27 @@
         <carousal style="height: 70vw;">
             
         </carousal>
-        <div class="w-50" style="position: absolute;left: 3em;top: 6em;font-size: 3vw;">
-        <div class="input-group">
-       <input type="text" class="form-control" placeholder="Delivery Reference Number" aria-label="Delivery Reference Number" aria-describedby="button-addon2">
-  <div class="input-group-append">
-    <button class="btn btn-outline-secondary" type="button" id="button-addon2">Track</button>
-  </div>
-</div>
-        </div>
-        <h1 style="position: absolute;top: 3em;left: 3em;color: white;font-size: 3vw;">
+        <div class="w-50" style="position: absolute;width: 100%;left: 3em;top: 3em;font-size: 3vw;">
+        
+        
+        
+        <h1 style="color: white;font-size: 3vw;">
         
             NO #01 Delivery Service <br> in Sri Lanka.
         
         </h1>
-       
+        <h3  style="color: white;fomnt-size: 1vw;">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin orci ipsum, accumsan eu elementum ut, tristique id sem. Aenean hendrerit et elit sit amet dapibus. Donec dictum gravida turpis placerat lobortis.
+      </h3>
+      <div class="input-group">
+       <input type="text" class="form-control" placeholder="Delivery Reference Number" aria-label="Delivery Reference Number" aria-describedby="button-addon2">
+        <div class="input-group-append">
+
+          <button class="btn btn-outline-secondary" type="button" id="button-addon2">Track</button>
+        </div>
+        
+</div>
+       </div>
         </div>
 
         <div>
